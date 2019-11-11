c26148b5e9fd70adab19a92f31533f98dda75bd7
cleancode <reliablesoft881121@outlook.com>
2019-11-11 07:20:45 +0300

    init GoogleStorageModule, add app icons.

---
+++ SharePhoto/GoogleStorage/GSExplorerController.swift
@@ -0,0 +1,249 @@
+//
+//  MainViewController.swift
+//  SharePhoto
+//
+//  Created by Admin on 11/7/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+import GoogleSignIn
+import GoogleAPIClientForREST
+import GTMSessionFetcher
+import Firebase
+import FirebaseStorage
+
+class GSExplorerController: UITableViewController {
+
+    @IBOutlet var fileListView: UITableView!
+    
+    var folderID: String?
+    var dataContents: [GTLRDrive_File]?
+    let activityView = ActivityView()
+    
+    func setFolderID(_ folderID:String) {
+        self.folderID = folderID
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        createNavigationButton()
+    }
+    
+    func createNavigationButton() {
+        let buttonSize: CGFloat = 36
+        
+        let button1 = UIButton(type: .custom)
+        button1.setImage(UIImage(named: "newfolder"), for: .normal)
+        button1.addTarget(self, action: #selector(onCreateNewFolder), for: .touchUpInside)
+        button1.frame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        button1.widthAnchor.constraint(equalToConstant: buttonSize).isActive = true
+        button1.heightAnchor.constraint(equalToConstant: buttonSize).isActive = true
+        let barButton1 = UIBarButtonItem(customView: button1)
+        
+        let button2 = UIButton(type: .custom)
+        button2.setImage(UIImage(named: "uploadphoto"), for: .normal)
+        button2.addTarget(self, action: #selector(onUploadPhoto), for: .touchUpInside)
+        button2.frame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        button2.widthAnchor.constraint(equalToConstant: buttonSize).isActive = true
+        button2.heightAnchor.constraint(equalToConstant: buttonSize).isActive = true
+        let barButton2 = UIBarButtonItem(customView: button2)
+        
+        self.navigationItem.rightBarButtonItems = [barButton2, barButton1]
+    }
+    
+    func createNewFolder(_ name: String?) {
+        if name == nil || name == "" {
+            return
+        }
+        
+        var folderID = "root"
+        if self.folderID != nil {
+            folderID = self.folderID!
+        }
+        
+        activityView.showActivityIndicator(self.view, withTitle: "Loading...")
+        GDModule.createFolder(name: name!, parentFolderID: folderID) { (folderID) in
+            self.activityView.hideActivitiIndicator()
+            self.loadFileList()
+        }
+    }
+    
+    @objc func onCreateNewFolder() {
+        //1. Create the alert controller.
+        let alert = UIAlertController(title: "", message: "Input new folder name", preferredStyle: .alert)
+
+        //2. Add the text field. You can configure it however you need.
+        alert.addTextField { (textField) in
+            textField.text = ""
+        }
+
+        // 3. Grab the value from the text field, and print it when the user clicks OK.
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+            let textField = alert!.textFields![0] // Force unwrapping because we know it exists.
+            self.createNewFolder(textField.text)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+        // 4. Present the alert.
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @objc func onUploadPhoto() {
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        if self.isMovingFromParent {
+            if self.folderID == nil {
+                GIDSignIn.sharedInstance()?.signOut()
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated:Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        loadFileList()
+    }
+    
+    func loadCloud() {
+        // Get a reference to the storage service using the default Firebase App
+        let storage = Storage.storage()
+
+        // Create a storage reference from our storage service
+        let storageRef = storage.reference()
+        let centralRef = storageRef.child("central")
+        
+        
+        
+        centralRef.listAll() { (result, error) in
+            if let error = error {
+                //...
+            }
+            
+            for prefix in result.prefixes {
+                
+            }
+            
+            for item in result.items {
+                
+            }
+        }
+    }
+    
+    func loadFileList() {
+        loadCloud()
+        
+        var folderID = "root"
+        if self.folderID != nil {
+            folderID = self.folderID!
+        }
+        
+        activityView.showActivityIndicator(self.view, withTitle: "Loading...")
+        
+        GDModule.listFiles(folderID) { (fileList, error) in
+            self.activityView.hideActivitiIndicator()
+
+            if error != nil {
+                
+            } else {
+                self.dataContents = fileList!.files
+                self.tableView.reloadData()
+            }
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if self.dataContents == nil {
+            return 0
+        }
+        
+        return self.dataContents!.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FileCell", for:indexPath) as! FileTableViewCell
+        
+        let file = self.dataContents![indexPath.row]
+        
+        if file.mimeType! == "application/vnd.google-apps.folder" {
+            cell.imgThumb.image = UIImage.init(named: "folder_icon")
+        } else {
+            
+            GDModule.downloadImageFile(file.identifier!) { (image) in
+                cell.imgThumb.image = image
+            }
+            
+            //let imageUrl = "https://drive.google.com/uc?export=view&id=\(file.identifier!)"
+            //cell.imgThumb.loadImageUsingCache(withUrl: imageUrl)
+            //cell.imgThumb.loadImageUsingCache(withUrl: "https://cdn.arstechnica.net/wp-content/uploads/2018/06/macOS-Mojave-Dynamic-Wallpaper-transition.jpg")
+        }
+        
+        cell.lblTitle.text = file.name!
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func deleteRow(_ rowIndex: Int) {
+        var actions: [(String, UIAlertAction.Style)] = []
+        actions.append(("Delete", UIAlertAction.Style.default))
+        actions.append(("Cancel", UIAlertAction.Style.cancel))
+
+        //self = ViewController
+        Alerts.showActionsheet(viewController: self, title: "Warning", message: "Are you sure you delete this image?", actions: actions) { (index) in
+            print("call action \(index)")
+            
+            if index == 0 {
+                self.dataContents!.remove(at: rowIndex)
+                self.tableView.deleteRows(at: [IndexPath.init(row: rowIndex, section: 0)], with: .automatic)
+            }
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            // handle delete (by removing the data from your array and updating the tableview)
+            self.deleteRow(indexPath.row)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let file = self.dataContents![indexPath.row]
+        
+        if file.mimeType! == "application/vnd.google-apps.folder" {
+            if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainVC") as? GDExplorerController
+            {
+                vc.setFolderID(file.identifier!)
+                navigationController?.pushViewController(vc, animated: true)
+            }
+        } else {
+        }
+    }
+}
---
+++ SharePhoto/GoogleStorage/GSModule.swift
@@ -0,0 +1,205 @@
+//
+//  GDModule.swift
+//  SharePhoto
+//
+//  Created by Admin on 11/6/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+import GoogleSignIn
+import GoogleAPIClientForREST
+import GTMSessionFetcher
+
+class GSModule: NSObject {
+    static let service = GTLRDriveService()
+    static var user: GIDGoogleUser?
+    static var uploadFolderID: String?
+    
+    static let imageCache = NSCache<NSString, UIImage>()
+    
+    // listing files
+    static func search(_ fileName: String, onCompleted: @escaping (String?, Error?) -> ()) {
+        let query = GTLRDriveQuery_FilesList.query()
+        query.pageSize = 1
+        query.q = "name contains '\(fileName)'"
+            
+        service.executeQuery(query) { (ticket, results, error) in
+            onCompleted((results as? GTLRDrive_FileList)?.files?.first?.identifier, error)
+        }
+    }
+    
+    static func listFiles(_ folderID: String, onCompleted: @escaping (GTLRDrive_FileList?, Error?) -> ()) {
+        
+        let query = GTLRDriveQuery_FilesList.query()
+        
+        if self.user == nil {
+            return
+        }
+        
+        //query.pageSize = 100
+
+        // Comma-separated list of areas the search applies to. E.g., appDataFolder, photos, drive.
+        query.spaces = "drive"
+        
+        // Comma-separated list of access levels to search in. Some possible values are "user,allTeamDrives" or "user"
+        query.corpora = "user"
+
+        let fileFilter = "(mimeType = 'application/vnd.google-apps.folder' or mimeType = 'image/jpeg' or mimeType = 'image/png') and trashed=false"
+
+        //let ownedByUser = "'\(user!.profile!.email!)' in owners"
+        
+        //query.q = "'root' in parents"
+        //query.q = "'\(folderID)' in parents"
+        //query.q = "\(foldersOnly) and \(ownedByUser)"
+        query.q = "\(fileFilter) and '\(folderID)' in parents"
+            
+        service.executeQuery(query) { (ticket, result, error) in
+            onCompleted(result as? GTLRDrive_FileList, error)
+        }
+    }
+    
+    static func listFilesInFolder(_ folder: String, onCompleted: @escaping (GTLRDrive_FileList?, Error?) -> ()) {
+        search(folder) { (folderID, error) in
+            guard let ID = folderID else {
+                onCompleted(nil, error)
+                return
+            }
+            self.listFiles(ID, onCompleted: onCompleted)
+        }
+    }
+    
+    static func downloadImageFile(_ fileID: String, onCompleted: @escaping (UIImage?) -> ()) {
+        // check cached image
+        if let cachedImage = self.imageCache.object(forKey: fileID as NSString)  {
+            onCompleted(cachedImage)
+        }
+
+        let imageUrl = "https://www.googleapis.com/drive/v3/files/\(fileID)?alt=media"
+        //let imageUrl = "https://drive.google.com/uc?export=view&id=\(fileID)"
+        let fetcher = service.fetcherService.fetcher(withURLString: imageUrl)
+        fetcher.beginFetch { (data, error) in
+            if error != nil {
+                onCompleted(nil)
+            } else {
+                let image = UIImage.init(data: data!)
+                if image != nil {
+                    self.imageCache.setObject(image!, forKey: fileID as NSString)
+                }
+                onCompleted(image)
+            }
+        }
+    }
+
+    static func populateFolderID() {
+        let myFolderName = "SharedPhoto"
+        getFolderID(
+            name: myFolderName,
+            service: service,
+            user: user!) { folderID in
+            if folderID == nil {
+                self.createFolder(name: myFolderName, parentFolderID: "root") {
+                    self.uploadFolderID = $0
+                }
+            } else {
+                // Folder already exists
+                self.uploadFolderID = folderID
+            }
+        }
+    }
+    
+    static func getFolderID(
+        name: String,
+        service: GTLRDriveService,
+        user: GIDGoogleUser,
+        completion: @escaping (String?) -> Void) {
+        
+        let query = GTLRDriveQuery_FilesList.query()
+
+        // Comma-separated list of areas the search applies to. E.g., appDataFolder, photos, drive.
+        query.spaces = "drive"
+        
+        // Comma-separated list of access levels to search in. Some possible values are "user,allTeamDrives" or "user"
+        query.corpora = "user"
+            
+        let withName = "name = '\(name)'" // Case insensitive!
+        let foldersOnly = "mimeType = 'application/vnd.google-apps.folder'"
+        let ownedByUser = "'\(user.profile!.email!)' in owners"
+        query.q = "\(withName) and \(foldersOnly) and \(ownedByUser)"
+        
+        service.executeQuery(query) { (_, result, error) in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+                                     
+            let folderList = result as! GTLRDrive_FileList
+
+            // For brevity, assumes only one folder is returned.
+            completion(folderList.files?.first?.identifier)
+        }
+    }
+    
+    static func createFolder(
+        name: String,
+        parentFolderID: String,
+        completion: @escaping (String) -> Void) {
+        
+        let folder = GTLRDrive_File()
+        folder.mimeType = "application/vnd.google-apps.folder"
+        folder.name = name
+        folder.parents = [parentFolderID]
+        
+        // Google Drive folders are files with a special MIME-type.
+        let query = GTLRDriveQuery_FilesCreate.query(withObject: folder, uploadParameters: nil)
+        
+        self.service.executeQuery(query) { (_, file, error) in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            
+            let folder = file as! GTLRDrive_File
+            completion(folder.identifier!)
+        }
+    }
+    
+    static func uploadFile(
+        name: String,
+        folderID: String,
+        fileURL: URL,
+        mimeType: String,
+        service: GTLRDriveService) {
+        
+        let file = GTLRDrive_File()
+        file.name = name
+        file.parents = [folderID]
+        
+        // Optionally, GTLRUploadParameters can also be created with a Data object.
+        let uploadParameters = GTLRUploadParameters(fileURL: fileURL, mimeType: mimeType)
+        
+        let query = GTLRDriveQuery_FilesCreate.query(withObject: file, uploadParameters: uploadParameters)
+        
+        service.uploadProgressBlock = { _, totalBytesUploaded, totalBytesExpectedToUpload in
+            // This block is called multiple times during upload and can
+            // be used to update a progress indicator visible to the user.
+        }
+        
+        service.executeQuery(query) { (_, result, error) in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            
+            // Successful upload if no error is returned.
+        }
+    }
+    
+    static func uploadMyFile() {
+        let fileURL = Bundle.main.url(
+            forResource: "my-image", withExtension: ".png")
+        uploadFile(
+            name: "my-image.png",
+            folderID: uploadFolderID!,
+            fileURL: fileURL!,
+            mimeType: "image/png",
+            service: service)
+    }
+}
