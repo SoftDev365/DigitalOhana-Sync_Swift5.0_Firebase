d1303c41a3bea9bc9ef338b2346c1135f17b9a00
cleancode <reliablesoft881121@outlook.com>
2019-12-03 06:17:34 +0300

    [update]  Hide StatusBar at Shared Storage Gallery

--- SharePhoto/PhotoGallery/GSGalleryVC.swift
+++ SharePhoto/PhotoGallery/GSGalleryVC.swift
@@ -16,6 +16,7 @@ class GSGalleryVC: UIViewController, UIScrollViewDelegate {
     var photoList: [[String:Any]]?
     var imgViewList: [ImageZoomView]?
     var curPage: Int = 0
+    var bIsFullscreen = false
     
     override open var shouldAutorotate: Bool {
         return true
@@ -37,7 +38,6 @@ class GSGalleryVC: UIViewController, UIScrollViewDelegate {
         self.view.addGestureRecognizer(tap)
         
         self.contentView.removeFromSuperview()
-        //self.tabBarController?.tabBar.isHidden = true
     }
     
     @objc func handleTap(sender: UITapGestureRecognizer) {
@@ -45,7 +45,14 @@ class GSGalleryVC: UIViewController, UIScrollViewDelegate {
             return
         }
         
-        self.navigationController!.isNavigationBarHidden = !self.navigationController!.isNavigationBarHidden
+        bIsFullscreen = !bIsFullscreen
+
+        self.navigationController!.isNavigationBarHidden = bIsFullscreen
+        setNeedsStatusBarAppearanceUpdate()
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return bIsFullscreen
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -80,44 +87,45 @@ class GSGalleryVC: UIViewController, UIScrollViewDelegate {
         self.scrView.isPagingEnabled = true
         self.scrView.delegate = self
         
-        self.relayoutImageItemViews(self.scrView.bounds.size)
+        self.relayoutImageViews(false)
     }
     
-    func relayoutImageItemViews(_ size: CGSize) {
-        if self.imgViewList == nil {
-            return
-        }
-
+    func relayoutImageViews(_ recalcZoomScale:Bool) {
+        guard let imgViewList = self.imgViewList else { return }
+        
+        let size = self.scrView.bounds.size
         let w = size.width
         let h = size.height
-
-        for i in 0...(self.imgViewList!.count-1) {
+        
+        for i in 0...(imgViewList.count-1) {
+            let item = imgViewList[i]
             let rect = CGRect(x: w*CGFloat(i), y: 0, width: w, height: h)
-            //self.imgViewList![i].setZoomScale(1.0, animated: false)
-            self.imgViewList![i].frame = rect
-            //self.imgViewList![i].setZoomScale(1.0, animated: false)
-            self.imgViewList![i].fitViewSizeToImage()
+            item.frame = rect
+            
+            if recalcZoomScale {
+                item.recalcZoomScale()
+            }
         }
         
-        //self.contentView.frame = CGRect(x: 0, y: 0, width: w*CGFloat(self.imgViewList!.count), height: h)
-        self.scrView.contentSize = CGSize(width: w*CGFloat(self.imgViewList!.count), height: h)
+        self.scrView.contentSize = CGSize(width: w*CGFloat(imgViewList.count), height: h)
         self.scrView.contentOffset = CGPoint(x: w*CGFloat(curPage), y: 0)
         self.scrView.isPagingEnabled = true
         self.scrView.delegate = self
     }
     
+    func recalcZoomScaleOfPhotos() {
+        relayoutImageViews(true)
+    }
+    
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        if self.imgViewList == nil {
-            return
-        }
-        
+        guard let imgViewList = self.imgViewList else { return }
+
         curPage = (Int)(self.scrView.contentOffset.x / self.scrView.bounds.width)
         
-        for i in 0...(imgViewList!.count-1) {
-            let item = imgViewList![i]
+        for i in 0...(imgViewList.count-1) {
+            let item = imgViewList[i]
             
             if i != curPage {
-                //item.setZoomScale(1.0, animated: false)
                 item.fitViewSizeToImage()
             }
             
@@ -126,21 +134,19 @@ class GSGalleryVC: UIViewController, UIScrollViewDelegate {
             }
         }
     }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-    }
-    
+
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
-        
-        //self.relayoutImageItemViews(size)
+
         curPage = (Int)(self.scrView.contentOffset.x / self.scrView.bounds.width)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        recalcZoomScaleOfPhotos()
+    }
+    
+    @IBAction func onBtnDownload(_ sender: Any) {
         
-        self.relayoutImageItemViews(self.scrView.bounds.size)
     }
 }
