aaef89d5b4325992c257f1db73245aa1fd82dbb0
cleancode <reliablesoft881121@outlook.com>
2019-12-24 05:43:36 +0300

    [update] modify ActivityView to fit the width for the title.

--- SharePhoto/Common/ActivityView.swift
+++ SharePhoto/Common/ActivityView.swift
@@ -9,6 +9,22 @@
 import Foundation
 import UIKit
 
+extension String {
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+
+        return ceil(boundingBox.height)
+    }
+
+    func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+
+        return ceil(boundingBox.width)
+    }
+}
+
 class ActivityView {
 
     var activityIndicator = UIActivityIndicatorView()
@@ -16,7 +32,8 @@ class ActivityView {
     let effectView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
     
     func relayoutPosition(_ view: UIView) {
-        effectView.frame = CGRect(x: view.frame.midX - 60, y: view.frame.midY - 60 , width: 120, height: 120)
+        let width = effectView.frame.size.width
+        effectView.frame = CGRect(x: view.frame.midX - width/2, y: view.frame.midY - 60 , width: width, height: 120)
         effectView.layer.cornerRadius = 15
         effectView.layer.masksToBounds = true
     }
@@ -24,14 +41,20 @@ class ActivityView {
     func showActivityIndicator(_ view: UIView, withTitle title: String) {
 
         hideActivitiIndicator()
+        
+        let widthOfTitle = title.width(withConstrainedHeight: 46, font: .systemFont(ofSize: 18, weight: UIFont.Weight.medium))
+        var widthOfView: CGFloat = 120
+        if widthOfView < widthOfTitle + 30 {
+            widthOfView = widthOfTitle + 30
+        }
 
-        strLabel = UILabel(frame: CGRect(x: 0, y: 70, width: 120, height: 46))
+        strLabel = UILabel(frame: CGRect(x: 0, y: 70, width: widthOfView, height: 46))
         strLabel.text = title
         strLabel.textAlignment = .center
         strLabel.font = .systemFont(ofSize: 18, weight: UIFont.Weight.medium)
         strLabel.textColor = UIColor(white: 0.9, alpha: 0.7)
 
-        effectView.frame = CGRect(x: view.frame.midX - 60, y: view.frame.midY - 60 , width: 120, height: 120)
+        effectView.frame = CGRect(x: view.frame.midX - widthOfView/2, y: view.frame.midY - 60 , width: widthOfView, height: 120)
         effectView.layer.cornerRadius = 15
         effectView.layer.masksToBounds = true
 
@@ -43,7 +66,7 @@ class ActivityView {
         }
         
         activityIndicator.color = UIColor.white
-        activityIndicator.frame = CGRect(x: 30, y: 20, width: 60, height: 60)
+        activityIndicator.frame = CGRect(x: (widthOfView - 60)/2, y: 20, width: 60, height: 60)
         activityIndicator.startAnimating()
 
         effectView.contentView.addSubview(activityIndicator)
